3c8e44db7fc99ac0aa5d28a54d425cce402b1951
Averta047 <97875482+Averta047@users.noreply.github.com>
2024-11-11 12:41:00 +0100

    11.11.24 - testing SDL2 with LUA

--- src/projects/engine/Main.cpp
+++ src/projects/engine/Main.cpp
@@ -1,75 +1,42 @@
 #include <Windows.h>
 
 #include <SDL.h>
-#include <stdio.h>
+#include <SDL_ttf.h>
 
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+#include <stdio.h>
+#include "lua5/lua.hpp"
+#include "LuaBridge/LuaBridge.h"
+
+void ShowMsg(const char* title, const char* message)
+{
+    MessageBoxA(nullptr, message, title, MB_OK);
+}
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pScmdline, int iCmdshow)
 {
-    //The window we'll be rendering to
-    SDL_Window* window = NULL;
+    lua_State* L = luaL_newstate();
+    luaL_openlibs(L);
 
-    //The surface contained by the window
-    SDL_Surface* screenSurface = NULL;
+    luabridge::getGlobalNamespace(L)
+        .beginNamespace("SDL")
+            .addFunction("Init", SDL_Init)
+            .addFunction("MessageBox", ShowMsg)
+            .addFunction("Quit", SDL_Quit)
+            .addFunction("CreateWindow", SDL_CreateWindow)
+            .addFunction("CreateRenderer", SDL_CreateRenderer)
+            .addFunction("PollEvent", SDL_PollEvent)
+            .addFunction("Delay", SDL_Delay)
+            .addFunction("DestroyWindow", SDL_DestroyWindow)
+            .addFunction("DestroyRenderer", SDL_DestroyRenderer)
+            .addFunction("GetError", SDL_GetError)
+        .endNamespace();
 
-    SDL_Surface* gHelloWorld = NULL;
-
-    //Initialize SDL
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
-    {
-        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-    }
-    else
-    {
-        //Create window
-        window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-        if (window == NULL)
-        {
-            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-        }
-        else
-        {
-            //Get window surface
-            screenSurface = SDL_GetWindowSurface(window);
-
-            gHelloWorld = SDL_LoadBMP("hello_world.bmp");
-            if (gHelloWorld == NULL)
-            {
-                printf("Unable to load image %s! SDL Error: %s\n", "02_getting_an_image_on_the_screen/hello_world.bmp", SDL_GetError());
-            }
-
-            //Fill the surface white
-            SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-
-            SDL_BlitSurface(gHelloWorld, NULL, screenSurface, NULL);
-
-            //Update the surface
-            SDL_UpdateWindowSurface(window);
-
-            //Hack to get window to stay up
-            SDL_Event e; 
-            bool quit = false; 
-            while (quit == false) 
-            { 
-                while (SDL_PollEvent(&e)) 
-                { 
-                    if (e.type == SDL_QUIT) 
-                        quit = true; 
-                } 
-            }
-        }
+    if (luaL_dofile(L, "game.lua")) {
+        MessageBoxA(nullptr, lua_tostring(L, -1), "KiwiEngine - LUA Error", MB_OK | MB_ICONERROR);
+        lua_pop(L, 1);
     }
 
-    SDL_FreeSurface(gHelloWorld);
-    gHelloWorld = NULL;
-
-    //Destroy window
-    SDL_DestroyWindow(window);
-
-    //Quit SDL subsystems
-    SDL_Quit();
+    lua_close(L);
 
 	return EXIT_SUCCESS;
 }
